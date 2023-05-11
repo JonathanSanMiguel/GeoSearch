a8ed4817170e19ec8c5d8f24b146fac371720e3a
Jonathan San Miguel <guerreroz80@outlook.es>
2023-05-11 14:33:35 -0600

    se crearon los componentes para las vistas y su respectivo routing

--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
-import { ByCountryComponent } from './pages/by-country/by-country.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
 
 const routes: Routes = [
     { path: 'byCapital', component: ByCapitalPageComponent },
-    { path: 'byCountry', component: ByCountryComponent }
+    { path: 'byCountry', component: ByCountryPageComponent },
+    { path: 'byRegion', component: ByRegionPageComponent },
+    { path: 'Country', component: CountryPageComponent }
 ]
 
 @NgModule({
